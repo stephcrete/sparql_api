ce0f7faf5f469444a160b7a1d569419a0a47aa51
stephcrete <62345611+stephcrete@users.noreply.github.com>
2023-11-20 15:26:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@
 from flask import render_template, request
 import connexion
 import config, sparql_query_templates, sparql_functions
+from OpenSSL import SSL
+context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
+context.use_privatekey_file('/root/server.key')
+context.use_certificate_file('/root/server.crt')
 
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("swagger.yml")
@@ -12,6 +16,6 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, ssl_context=('/root/server.crt', '/root/server.key'))
+    app.run(host="0.0.0.0", port=8000, ssl_context=context)
 
 
