d57291a00fffd3ebb15f76aa36f90b7d4e67fece
stephcrete <62345611+stephcrete@users.noreply.github.com>
2023-11-21 16:55:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, ssl_keyfile="server.key",
+    app.run(host="0.0.0.0", port=8080, ssl_keyfile="server.key",
                ssl_certfile="server.crt")
 
 
