edb3e9165970367625913995dfca3cd771de5a71
stephcrete <62345611+stephcrete@users.noreply.github.com>
2023-11-23 17:05:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 # app.py
 
-from flask import render_template, request
-import connexion
-import config, sparql_query_templates, sparql_functions
+#from flask import render_template, request
+#import connexion
+#import config, sparql_query_templates, sparql_functions
+from flask import Flask
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
-
+#app = connexion.App(__name__, specification_dir="./")
+#app.add_api("swagger.yml")
+app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
 
 
